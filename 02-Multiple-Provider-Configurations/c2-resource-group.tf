5733d3c33d607fecb7608724b0348151e87c036d
SachinPitale <sachinpitale22@gmail.com>
2021-09-21 11:42:59 +0530

    update resource.tf for mul provider

--- 02-Multiple-Provider-Configurations/c2-resource-group.tf
+++ 02-Multiple-Provider-Configurations/c2-resource-group.tf
@@ -1,7 +1,12 @@
 # Create a resource group in EastUS region - Uses Default Provider
-resource "azurerm_resource_group " "devrg1" {
-    name = "rg-1"
-    location = "East US"
+#resource "azurerm_resource_group " "devrg1" {
+#    name = "rg-1"
+#    location = "East US"
+#}
+
+resource "azurerm_resource_group" "myrg1" {
+  name = "myrg-1"
+  location = "East US"
 }
 
 #Create a resource group in WestUS region - Uses "provider2-westus" provider
