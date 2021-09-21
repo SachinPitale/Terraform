8e81fa7ef0e3f0c8485af3e9692bd73a5196a290
SachinPitale <sachinpitale22@gmail.com>
2021-09-21 12:23:18 +0530

    add  subnet and public ip

--- 03-Meta-Argument-depends_on/c3-virtual_network.tf
+++ 03-Meta-Argument-depends_on/c3-virtual_network.tf
@@ -1,5 +1,30 @@
-resource "azurerm_virtaul_network" "uatvnet" {
+resource "azurerm_virtual_network" "uatvnet" {
   name = "Uat-vnet"
   location = azurerm_resource_group.myrg1.location
   resource_group_name = azurerm_resource_group.myrg1.name
+  address_space = [ "10.10.0.0/16" ]
+}
+
+resource "azurerm_subnet" "websubnet" {
+  name = "websunbet"
+  resource_group_name =  azurerm_resource_group.myrg1.name
+  virtual_network_name = azurerm_virtual_network.uatvnet
+  address_prefixes = [ "10.10.1.0/24" ]
+
+}
+
+resource "azurerm_public_ip" "web1" {
+  depends_on = [
+    azurerm_virtual_network.uatvnet,
+    azurerm_subnet.websubnet  
+  ]
+  name = "web1"
+  resource_group_name = azurerm_resource_group.myrg1.name
+  location = azurerm_resource_group.myrg1.location
+  allocation_method = "static"
+  domain_name_label = "web1-vm-${random_string.myrandom.id}"
+  tags = {
+    environemt = UAT
+  }
+  
 }
