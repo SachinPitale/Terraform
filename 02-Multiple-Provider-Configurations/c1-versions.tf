23e8fb60d22aefc05c8f74d867af1ed66cce5dc1
SachinPitale <sachinpitale22@gmail.com>
2021-09-21 11:36:38 +0530

    update version.tf for mul provider

--- 02-Multiple-Provider-Configurations/c1-versions.tf
+++ 02-Multiple-Provider-Configurations/c1-versions.tf
@@ -28,8 +28,6 @@ provider "azurerm" {
         virtaul_machine {
             delete_os_disk_on_deletion  = false #  This will ensure when the Virtual Machine is destroyed, Disk is not deleted, default is true and we can alter it at provider level
         }
-    alias = "provider2-westus"
-
     }
-
+    alias = "provider2-westus"
 }
