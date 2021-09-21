cc19bfd1f809da8c944129687b3dff1a1bca6c84
SachinPitale <sachinpitale22@gmail.com>
2021-09-21 11:40:18 +0530

    update resource.tf for mul provider

--- 02-Multiple-Provider-Configurations/c2-resource-group.tf
+++ 02-Multiple-Provider-Configurations/c2-resource-group.tf
@@ -1,13 +1,13 @@
 # Create a resource group in EastUS region - Uses Default Provider
 resource "azurerm_resource_group " "rg1" {
-    name = "rg1"
+    name = "rg-1"
     location = "East US"
 }
 
 #Create a resource group in WestUS region - Uses "provider2-westus" provider
 
 resource "azurerm_resource_group" "rg2" {
-    name = "rg2"
+    name = "rg-2"
     location = "West Us"
     provider = azurerm.provider2-westus #  This is a Meta-Argument from Resources Section nothing but a Special Argument
 }
