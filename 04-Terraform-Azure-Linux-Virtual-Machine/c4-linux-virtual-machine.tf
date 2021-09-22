d899f67769a8b3470b99bb598431563ff96f9397
SachinPitale <sachinpitale22@gmail.com>
2021-09-22 12:27:45 +0530

    add app script for vm

---
+++ 04-Terraform-Azure-Linux-Virtual-Machine/c4-linux-virtual-machine.tf
@@ -0,0 +1,27 @@
+# Resource: Azure Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "webserver" {
+  name = "webserver"
+  computer_name = "apache-webserver"
+  resource_group_name = azurerm_resource_group.myrg1.name
+  location = azurerm_resource_group.myrg1.location
+  size = "Standard_DS1_v2"
+  admin_username = "azureuser"
+  network_interface_ids = [ azurerm_network_interfac.web1-inc.id ]
+  admin_ssh_key {
+    username = "azureuser"
+    public_key = file("${path.module}.ssh-keys/terraform-azure.pub")
+  }
+  os_disk {
+    name = "osdisk"
+    caching = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+
+  }
+  source_image_reference {
+    publisher =  "RedHat"
+    offer = "RHEL"
+    sku = "83-gen2"
+    version = "latest"
+  }
+  custom_data = filebase("${path.module}/app-scripts/app1-cloud-init.txt")
+}
