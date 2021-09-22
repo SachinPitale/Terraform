3d399341d3fadf8987fcab2d8dc16c68f6cd665f
SachinPitale <sachinpitale22@gmail.com>
2021-09-22 12:55:56 +0530

    added count example1

--- 05-Meta-Argument-count/c1-versions.tf
+++ 05-Meta-Argument-count/c1-versions.tf
@@ -1,7 +1,7 @@
 # Terraform Block
 terraform {
   required_version = ">=1.0.0"
-  required_provider {
+  required_providers {
       azurerm = {
       source = "hashicorp/azurerm"
       version = ">= 2.0" 
--- 05-Meta-Argument-count/c2-resource-group.tf
+++ 05-Meta-Argument-count/c2-resource-group.tf
@@ -2,6 +2,6 @@
 # Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "devrg" {
   count = 3
-  name = devrg-${count.index} 
+  name = "devrg-${count.index}" 
   location =  "East US"
 }
