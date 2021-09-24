2d572b2a19e46a80890e80191614778de75a3ab3
SachinPitale <sachinpitale22@gmail.com>
2021-09-24 21:24:18 +0530

    Input-Variables-Basic

--- 12-Input-Variables-Basic/c2-resource_group.tf
+++ 12-Input-Variables-Basic/c2-resource_group.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "myrg1" {
-  name = "${var.business_unit}-${var.environment}-${var.resource_group_name}"
+  name = "${var.Business_Unit}-${var.environment}-${var.resource_group_name}"
   location = var.resource_group_location
 } 
--- 12-Input-Variables-Basic/c3-virtual_network.tf
+++ 12-Input-Variables-Basic/c3-virtual_network.tf
@@ -1,6 +1,6 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "uatvnet" {
-  name = "${var.business_unit}-${var.environment}-${var.virtual_network_name}"
+  name = "${var.Business_Unit}-${var.environment}-${var.resource_group_name}"
   location = azurerm_resource_group.myrg1.location
   resource_group_name = azurerm_resource_group.myrg1.name
   address_space = [ "10.10.0.0/16" ]
