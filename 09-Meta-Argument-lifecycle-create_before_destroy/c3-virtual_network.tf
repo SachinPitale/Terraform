3a21f82f04d0a5ca8e2c7ee1363eb2d92b7d5060
SachinPitale <sachinpitale22@gmail.com>
2021-09-24 11:59:23 +0530

    add lifecycle-create_before_destroy

--- 09-Meta-Argument-lifecycle-create_before_destroy/c3-virtual_network.tf
+++ 09-Meta-Argument-lifecycle-create_before_destroy/c3-virtual_network.tf
@@ -1,8 +1,13 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "uatvnet" {
-  name = "Uat-vnet"
+  name = "Uat-vnet-1"
   location = azurerm_resource_group.myrg1.location
   resource_group_name = azurerm_resource_group.myrg1.name
   address_space = [ "10.10.0.0/16" ]
+
+  # Lifecycle Changes
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
