727bb4260649153c364931e7fdcf582c38f08991
SachinPitale <sachinpitale22@gmail.com>
2021-09-25 16:07:31 +0530

    Input-Variables-Collection-Type-Maps

---
+++ 18-Input-Variables-Collection-Type-Maps/c3-virtual_network.tf
@@ -0,0 +1,53 @@
+# Create Virtual Network
+resource "azurerm_virtual_network" "uatvnet" {
+  name = "${var.Business_Unit}-${var.environment}-${var.resource_group_name}"
+  location = azurerm_resource_group.myrg1.location
+  resource_group_name = azurerm_resource_group.myrg1.name
+   address_space = var.virtual_network_address_space
+  #address_space = [var.virtual_network_address_space[0]]
+  tags =  var.common_tags
+}
+
+# Create Subnet
+resource "azurerm_subnet" "websubnet" {
+  name = "websunbet"
+  resource_group_name =  azurerm_resource_group.myrg1.name
+  virtual_network_name = azurerm_virtual_network.uatvnet.name
+  address_prefixes = [ "10.30.1.0/24" ]
+  tags =  var.common_tags
+}
+# Create Public IP Address
+resource "azurerm_public_ip" "web1" {
+  depends_on = [
+    azurerm_virtual_network.uatvnet,
+    azurerm_subnet.websubnet  
+  ]
+  name = "web1"
+  resource_group_name = azurerm_resource_group.myrg1.name
+  location = azurerm_resource_group.myrg1.location
+  allocation_method = "Static"
+  domain_name_label = "web1-vm-${random_string.myrandom.id}"
+  #sku = var.public_ip_sku["eastus"]
+  sku = lookup(var.public_ip_sku, var.resource_group_location, Basic) 
+  tags =  var.common_tags
+  
+}
+
+
+# Create Network Interface
+
+resource "azurerm_network_interface" "web1-inc" {
+  name = "web1-inc"
+  location = azurerm_resource_group.myrg1.location
+  resource_group_name = azurerm_resource_group.myrg1.name
+  ip_configuration  {
+    name = "internal"
+    subnet_id = azurerm_subnet.websubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.web1.id
+
+  }
+  tags =  var.common_tags
+}
+
+
---
+++ 18-Input-Variables-Collection-Type-Maps/c4-variables.tf
@@ -0,0 +1,70 @@
+# Input Variables
+# 1. Business Unit Name
+
+variable "Business_Unit" {
+    description = "Business Unit Name"
+    type = string 
+}
+
+# 2. Environment Name
+variable "environment" {
+    description = "Environment Name"
+    type = string
+}
+
+# 3. Resource Group Name
+variable "resource_group_name" {
+    description = "Resource Group Name"
+    type = string 
+}
+
+# 4. Resource Group Location
+variable "resource_group_location" {
+  description = "Resource Group Location"
+  type = string
+}
+
+
+# 5. Virtual Network Name
+variable "virtual_network_name" {
+  description = "Virtual Network Name"
+  type = string
+}
+
+# 6. Virtual Network address_space
+variable "virtual_network_address_space" {
+  description = "Virtual Network Address Space"
+  type = list(string)
+  default = [ "10.20.0.0/16","10.21.0.0/16","10.22.0.0/16" ]
+  
+}
+
+# 7. Subnet Name: Assign When Prompted using CLI
+variable "subnet_name" {
+  description = "Virtual Network Subnet Name"
+  type = string 
+}
+
+# 8. Public IP SKU
+
+variable "public_ip_sku" {
+  description = "Azure Public IP Address SKU"
+  type = map(string)
+  default = {
+    "eastus" = "Basic",
+    "eastus2" = "Standard"
+  }
+  
+}
+
+# 9. Common Tags
+
+variable "common_tags" {
+  description = "Common Tags for Azure Resources"
+  type = map(string)
+  default = {
+    "CLITool" = "Terraform",
+    "Tag1" = "Azure"
+  }
+  
+}
