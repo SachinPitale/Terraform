53f748d02293fbaac751a49d71314ba043cb8a28
SachinPitale <sachinpitale22@gmail.com>
2021-09-26 13:35:20 +0530

    Output-Values-with-for_each-and-for-loops

--- 26-Output-Values-with-for_each-and-for-loops/c3-resource-group.tf
+++ 26-Output-Values-with-for_each-and-for-loops/c3-resource-group.tf
@@ -1,6 +1,7 @@
 # Resource-1: Azure Resource Group
 resource "azurerm_resource_group" "myrg" {
-  name = "${var.business_unit}-${var.environment}-${var.resoure_group_name}"
+  for_each = var.environment
+  name = "${var.business_unit}-${each.key}-${var.resoure_group_name}"
   location = var.resoure_group_location
 }
 
