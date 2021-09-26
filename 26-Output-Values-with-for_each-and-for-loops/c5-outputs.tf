07f73cfd06f4c0e1c566418513744f2d59fae5e7
SachinPitale <sachinpitale22@gmail.com>
2021-09-26 13:53:08 +0530

    Output-Values-with-for_each-and-for-loops

--- 26-Output-Values-with-for_each-and-for-loops/c5-outputs.tf
+++ 26-Output-Values-with-for_each-and-for-loops/c5-outputs.tf
@@ -23,7 +23,7 @@ output "virtual_network_name" {
 # Output - For Loop One Input and List Output with VNET Name 
 output "virtual_network_name_list_one_input" {
   description = "Virtual Network - For Loop One Input and List Output with VNET Name"
-  value = [for vnet in azurerm_virtual_network.myvnet: vnet]
+  value = [for vnet in azurerm_virtual_network.myvnet: vnet.name]
   
 }
 
@@ -34,3 +34,32 @@ output "virtual_network_name_list_two_inputs" {
   value = [for env, vnet in azurerm_virtual_network.myvnet: env]
   
 }
+
+# Output - For Loop One Input and Map Output with VNET ID and VNET Name
+output "virtual_network_name_map_one_input" {
+  description = "Virtual Network - For Loop One Input and Map Output with VNET ID and VNET Name"
+  value = {for env, vnet in azurerm_virtual_network.myvnet: env => vnet.name }
+}
+
+# Output - For Loop Two Inputs and Map Output with Iterator env and VNET Name
+output "virtual_network_name_map_two_inputs" {
+  description = "Virtual Network - For Loop One Input and Map Output with VNET ID and VNET Name"
+  value = {for vnet in azurerm_virtual_network.myvnet: vnet.id => vnet.name }
+}
+
+# Output - For Loop Two Inputs and Map Output with Iterator env and VNET Name
+output "virtual_network_name_map_two_inputs" {
+  description = "Virtual Network - For Loop Two Inputs and Map Output with Iterator env and VNET Name"
+  value = {for env, vnet in azurerm_virtual_network.myvnet: env => vnet.name }
+}
+ 
+# Terraform keys() function: keys takes a map and returns a list containing the keys from that map.
+output "virtual_network_name_keys_function" {
+  description = "Virtual Network - Terraform keys() function"
+  value = keys({for env, vnet in azurerm_virtual_network.myvnet: env => vnet.name })
+}
+
+# Terraform values() function: values takes a map and returns a list containing the values of the elements in that map.
+output "virtual_network_name_values_function" {
+  description = "Virtual Network - Terraform values() function"
+  value = values({for env, vnet in azurerm_virtual_network.myvnet: env => vnet.name }) 
