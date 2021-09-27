62b2b4cfa549223d8ea6d567fc0b505c6ad3add7
SachinPitale <sachinpitale22@gmail.com>
2021-09-27 11:45:36 +0000

    resource condition

---
+++ 29-Terraform-Conditional-Expressionsterraform-Resources/c3-virtual-network.tf
@@ -0,0 +1,13 @@
+# Create Virtual Network
+resource "azurerm_virtual_network" "myvnet" {
+  count = var.environment == "dev" "? 1 : 3
+  name                = "${var.business_unit}-${var.environment}-${var.virtual_network_name}-${count.index}"
+  #address_space       = ["10.0.0.0/16"]
+  address_space       =  local.vnet_address_space
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+  tags = local.common_tags
+}
+
+
+
